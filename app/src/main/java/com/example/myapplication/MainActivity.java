0242283fc110c4448c01bea70b31d44d1bc4b9b5
CARRERA PROFESIONAL <pieroronaldo024@gmail.com>
2019-09-06 17:31:53 -0500

    hola

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView estado;
+    private EditText email;
+    private  EditText contraseña;
+    private Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email=(EditText) findViewById(R.id.txtemail);
+        contraseña=(EditText) findViewById(R.id.txtcontraseña);
+        registrar=(Button)findViewById(R.id.btnregistrar);
+        estado=(TextView) findViewById(R.id.lblestado);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               
+            }
+        });
     }
 }
+
