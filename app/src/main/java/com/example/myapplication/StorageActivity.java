62d139ce43a3789d1fe1e604d1e9fe2dc224a110
CARRERA PROFESIONAL <pieroronaldo024@gmail.com>
2019-09-26 17:31:41 -0500

    asdasd

--- app/src/main/java/com/example/myapplication/StorageActivity.java
+++ app/src/main/java/com/example/myapplication/StorageActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class StorageActivity extends AppCompatActivity {
+private Button Upload;
+private static  final int GALLERY_INTENT = 3;
+    private StorageReference storage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
-    }
-}
+
+        Upload=(Button)findViewById(R.id.uploadimg);
+        storage= FirebaseStorage.getInstance().getReference();
+      /*Upload.setOnClickListener((v)  →{
+            Intent intent=new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent,GALLERY_INTENT);
+        });*/
+
+    };
+
